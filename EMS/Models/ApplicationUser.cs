ada1204e9dc3065610b586b0671987ba00b995f8
Nguyễn Phú Đức <nguyenphuduc62001@gmail.com>
2024-07-18 11:36:34 +0700

    initial

---
+++ EMS/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EMS.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    
+}
+
---
+++ EMS/Pages/Shared/_CookieConsentPartial.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EMS.Pages.Shared;
+
+public class _CookieConsentPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
