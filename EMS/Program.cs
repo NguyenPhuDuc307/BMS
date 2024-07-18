1498ae64c18cacfa613003bbcdbe181223cadd06
Nguyễn Phú Đức <nguyenphuduc62001@gmail.com>
2024-07-18 11:36:43 +0700

    initial

--- EMS/Program.cs
+++ EMS/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
